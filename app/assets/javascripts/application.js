9ae0516c1310d5343eb8b9db50a257e84ee46213
benfurber <ben@furber-consultancy.com>
2018-01-17 19:29:45 +0000

    Socify setup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,26 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
+// compiled file.
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require turbolinks
+//= require jquery
+//= require jquery.atwho
+//= require jquery_ujs
+//= require bootstrap
+//= require bindWithDelay
+//= require moment.min
+//= require fullcalendar.min
+//= require jquery.datetimepicker
 //= require_tree .
+
+$(document).ready(function () {
+  $("#event_event_datetime").datetimepicker({format: 'Y/m/d H:i'});
+  $("#user_dob").datetimepicker({timepicker: false, format: 'Y/m/d', maxDate: '0'});
+});
